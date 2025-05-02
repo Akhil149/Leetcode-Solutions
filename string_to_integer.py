dd7b97aeee4df273168730e7c41d795574a88781
Akhileshwar Reddy <aakhil.avula@gmail.com>
2025-05-02 17:13:35 -0400

    String to Integer(atoi) leetcode problem 8 solution

---
+++ string_to_integer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s=s.strip()
+        res_s = ""
+        if s == "" or s == "+" or s == '-':
+            return 0
+        for i in range(len(s)):
+            if (s[i] == '-' or s[i] == "+") and res_s == "":
+                if not s[i+1].isdigit():
+                    res_s += "0"
+                    break
+                res_s += s[i]
+            elif s[i].isdigit():
+                res_s += s[i]
+            else:
+                if res_s == "":
+                    res_s += "0"
+                break
+        res = int(res_s)
+        if res < pow(-2, 31):
+            res = pow(-2,31)
+        elif res > (pow(2, 31) - 1):
+            res = pow(2,31)-1
+        return res
